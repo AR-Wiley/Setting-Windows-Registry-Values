1376bc522ced396239458012a3c7e4eb608ea677
AR Wiley <98679479+AR-Wiley@users.noreply.github.com>
2025-10-30 21:02:12 -0500

    Add files via upload

---
+++ Always-DownloadPDFFiles.ps1
@@ -0,0 +1,29 @@
+﻿function Always-DownloadPDFFiles {
+
+    if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+         Write-Host 'Not running as admin'
+        return
+    }
+
+    else {
+
+        Write-Host 'Running as admin'
+
+    } 
+    
+    $RegistryPath = "HKCU:\Software\Policies\Microsoft\Edge"
+
+    if (!(Test-Path -Path $RegistryPath)) {
+         Write-Host "$RegistryPath does not exist"
+         return
+    }
+
+    Try {
+        New-ItemProperty -Path $RegistryPath -Name "AlwaysOpenPdfExternally" -Value 1 -PropertyType DWORD
+        Write-Host "Registry Updated"
+    } Catch {
+        Write-Host "Failed to update registry"
+        return
+    }
+
+} 
